0369d4744833021d2f7d072329fd6c4c1eebffc0
otabek <otabekbarotov15@gmail.com>
2026-03-11 14:16:54 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Program to determine whether the number is even or odd
+
+def even_odd(n: int) -> str:
+    if n % 2 == 0:
+        return "odd"
+    elif n % 2 != 0:
+        return "odd"
+    else:
+        return f"re-run the program"
